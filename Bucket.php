4a7cd6434d839a63594e8140ae67e1271edba050
denygaming <127093359+denygaming@users.noreply.github.com>
2023-03-07 17:16:35 -0800

    Add files via upload

---
+++ Bucket.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="assets/css/style.css" />
+  <title>JUARA DIGITAL PRINTING</title>
+  <style>
+      p{
+        display:block;
+      }
+    </style>
+<body>
+<header>
+    <img src="" alt="" class="" />
+    <nav class="topnavbar" id="Navbar">
+      <ul>
+        <li style="color: #e1fa04;">JUARA DIGITAL PRINTING</li>
+      </ul>
+    </nav>
+  </header>
+  <header>
+    <img src="" alt="" class="" />
+    <nav class="topNavbar" id="navbar">
+      <ul>
+        <li><a href="index.html" class="active">Rental Camera</a></li>
+        <li><a href="bucket.php">bucket</a></li>
+        <li><a href="Cindera Mata.php">Cindera Mata</a></li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <div id="pictumbnail" class="content">
+      <div class="tumbnail">
+        <div class="article-header">
+          <img src="assets/images/Foto.JPG" alt="dreadout_slider" />
+        </div>
+      </div>
+      <div id="article" class="container">
+        <div class="description-tumbnail">
+          <h2 class="header-tumbnail">Sedia :</h2>
+          <p>
+             - Bucket Bunga
+             - bucket uang
+             - bucket makanan 
+             - Dll
+          </p>
+         
